725c9a4aed7abadf3ab48ca7e298658e78dd17e6
Youssef <xie.zhongyong@163.com>
2018-04-13 23:27:43 +0800

    add NewDemo.java file

---
+++ src/file/KeyinTest.java
@@ -0,0 +1,14 @@
+package file;
+
+import java.io.InputStreamReader;
+
+public class KeyinTest {
+	public static void main(String[] args) {
+		//Using the system.in as a input and  we can transform it from byte to char
+		InputStreamReader isr = new InputStreamReader(System.in);
+		//make a container
+		char[] cbuff = new char[1024];
+		//a indicator
+
+	}
+}
